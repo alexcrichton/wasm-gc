7da3dc97b97c8d6aaaef59709a8f670c8515d22c
Alex Crichton <alex@alexcrichton.com>
2018-07-25 15:14:09 -0700

    Allow reusing the internal `parity_wasm::Module`
    This commit allows to avoid parsing and serializing a wasm module, if possible.
    This is one of the hot spots of `wasm-bindgen` right now, and it's already got
    the same version of `parity_wasm` so there's no need to do this reserialization
    and reparsing!
    
    This is done in a fashion, though, to avoid making `parity_wasm` a public
    dependency which is changing rapidly. Instead it uses `Any` to allow other
    crates to pass in an arbitrary type, but otherwise a fallback for going through
    bytes is always provided and possible.

--- wasm-gc-api/src/lib.rs
+++ wasm-gc-api/src/lib.rs
@@ -6,7 +6,10 @@ extern crate rustc_demangle;
 mod gc;
 mod error;
 
+use std::any::Any;
+use std::mem;
 use std::path::Path;
+
 use parity_wasm::elements::{
     Module,
     Serialize,
@@ -19,6 +22,8 @@ pub struct Config {
     demangle: bool,
     keep_debug: bool,
 }
+
+pub struct GcResult(Box<Module>);
 
 impl Config {
     /// Creates a blank slate of configuration, ready to gc wasm files.
@@ -44,20 +49,64 @@ impl Config {
 
     /// Runs gc passes over the wasm input module `input`, returning the
     /// serialized output.
-    pub fn gc(&mut self, mut bytecode: &[u8]) -> Result<Vec<u8>, Error> {
-        let mut module = Module::deserialize(&mut bytecode)
+    #[doc(hidden)] // deprecated, use `run` now.
+    pub fn gc(&mut self, bytecode: &[u8]) -> Result<Vec<u8>, Error> {
+        self.run(3, |_| bytecode.to_vec())?.into_bytes()
+    }
+
+    pub fn run<T: Any>(
+        &mut self,
+        mut module: T,
+        into_bytes: impl FnOnce(T) -> Vec<u8>,
+    ) -> Result<GcResult, Error> {
+        if let Some(module) = (&mut module as &mut Any).downcast_mut() {
+            self._gc(module);
+            let module = mem::replace(module, Module::new(Vec::new()));
+            return Ok(GcResult(Box::new(module)))
+        }
+        let bytecode = into_bytes(module);
+        let mut module = Module::deserialize(&mut &bytecode[..])
             .map_err(error::from)?
             .parse_names()
             .map_err(|(mut l, _)| l.remove(0).1)
             .map_err(error::from)?;
         self._gc(&mut module);
-        let mut output = Vec::new();
-        module.serialize(&mut output).map_err(error::from)?;
-        Ok(output)
+        Ok(GcResult(Box::new(module)))
     }
 
     fn _gc(&mut self, module: &mut Module) {
         gc::run(self, module);
+    }
+}
+
+impl GcResult {
+    /// Attepts to downcast this `GcResult` into an instance of
+    /// `parity_wasm::Module`.
+    ///
+    /// If your crate's `parity_wasm` crate is a different version than this
+    /// crate then this method will fail and you'll need to use `into_bytes`.
+    /// Otherwise the module is successfully extracted and returned.
+    pub fn into_module<T: Any>(self) -> Result<T, Self> {
+        let module = self.0 as Box<Any>;
+        match module.downcast() {
+            Ok(t) => Ok(*t),
+            Err(box_any) => {
+                match box_any.downcast::<Module>() {
+                    Ok(box_module) => Err(GcResult(box_module)),
+                    Err(_) => panic!(),
+                }
+            }
+        }
+    }
+
+    /// Convert this `GcResult` into a serialized wasm module.
+    ///
+    /// Returns any error that happens during serialization, which shouldn't
+    /// happen for valid modules.
+    pub fn into_bytes(self) -> Result<Vec<u8>, Error> {
+        let mut output = Vec::new();
+        self.0.serialize(&mut output).map_err(error::from)?;
+        Ok(output)
     }
 }
 
